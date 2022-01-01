dc8bce2ca4634039c89977a8c0a0e0055ec83a14
yonda-yonda <minerva.yooda@gmail.com>
2022-01-01 22:39:37 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import logo from "./logo.svg";
 
 import { useOl } from "~/hooks/useOl";
 import "./App.css";
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Contact() {
+  return <h1>Contact</h1>;
+}
 
 const ColoredLink = styled.a`
   color: red;
@@ -14,35 +23,56 @@ function App() {
   const ol = useOl();
   const [count, setCount] = React.useState<number>(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <ColoredLink
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => {
-            setCount((count) => {
-              return ++count;
-            });
+    <React.StrictMode>
+      <BrowserRouter>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
           }}
         >
-          Learn React {count}
-        </ColoredLink>
-      </header>
-      <div
-        ref={ol.ref}
-        style={{
-          width: "100%",
-          height: "480px",
-        }}
-      />
-      .....
-    </div>
+          <Link to="/">home</Link> | <Link to="/about">about</Link> |{" "}
+          <Link to="/contact">contact</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <ColoredLink
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => {
+                      setCount((count) => {
+                        return ++count;
+                      });
+                    }}
+                  >
+                    Learn React {count}
+                  </ColoredLink>
+                </header>
+                <div
+                  ref={ol.ref}
+                  style={{
+                    width: "100%",
+                    height: "480px",
+                  }}
+                />
+              </div>
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
